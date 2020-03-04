6d65809efb903ee3d7e160a754b205f31a39b9ea
Raj <rjariwala@mortgagealliance.com>
2020-03-04 12:23:04 -0500

    initializing files.

---
+++ Auth0-test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Auth0_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
